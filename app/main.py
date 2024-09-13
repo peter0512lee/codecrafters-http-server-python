5b557cf5c9cf8b0b686c1a37aea721f587e784b8
JYL <peter0512lee@gmail.com>
2024-09-13 22:50:47 +0800

    test

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-import socket  # noqa: F401
+import socket
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    server_socket: socket.socket = socket.create_server(
+        ("localhost", 4221), reuse_port=True
+    )
+    client: socket.socket
+    client, addr = server_socket.accept()
+    data: str = client.recv(1024).decode()
+    request_data: list[str] = data.split("\r\n")
+    response: bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
+    if request_data[0].split(" ")[1] != "/":
+        response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+    client.send(response)
+    client.close()
+    server_socket.close()
 
 
 if __name__ == "__main__":
